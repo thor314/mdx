b8c29d188644fe9c0fec0f9afafdc3c2ea191b1b
Thor Kamphefner <thorck@pm.me>
2023-02-28 09:43:02 -0700

    unhandled runtime error: element type invalid

---
+++ src/components/myComponents.tsx
@@ -0,0 +1,5 @@
+function MyComponent() {
+  return <div>My Component</div>;
+}
+
+export default MyComponent;
--- src/pages/myComponents.tsx
+++
@@ -1,3 +0,0 @@
-export default function MyComponent() {
-  return <div>My Component</div>;
-}
