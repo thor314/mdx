f203bc3c70c384e3dca7f8fb4cb4ba76394f3820
Thor Kamphefner <thorck@pm.me>
2023-02-28 09:32:42 -0700

    example failing

---
+++ src/my_components.tsx
@@ -0,0 +1,8 @@
+
+export default function MyComponents() {
+  return (
+    <div>
+      My Component
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,12 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <Link 
+            href="/example"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >example</Link>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
